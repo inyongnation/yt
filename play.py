22cc5c56e7eca2308458cb65a7c192ccc77aed32
InyongNation <71963003+inyongnation@users.noreply.github.com>
2023-12-24 10:50:59 +0800

    Update play.py

--- play.py
+++ play.py
@@ -1,6 +1,5 @@
 import subprocess
 import time
-import pyautogui
 
 def open_chrome_as_guest(video_url):
     try:
@@ -9,26 +8,6 @@ def open_chrome_as_guest(video_url):
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
-def minimize_chrome():
-    # Mencari proses Chrome dan mengirimkan sinyal untuk meminimalkan
-    try:
-        chrome_pid = int(subprocess.check_output(["pgrep", "chrome"]))
-        os.kill(chrome_pid, signal.SIGUSR1)  # Mengirimkan sinyal untuk meminimalkan
-    except (subprocess.CalledProcessError, ValueError):
-        print("Error minimizing Chrome")
-
-def resize_chrome(width, height):
-    # Menyesuaikan ukuran jendela Chrome dengan menggunakan pyautogui
-    pyautogui.hotkey('alt', 'space', 'r')  # Mengaktifkan menu jendela
-    time.sleep(0.5)
-    pyautogui.typewrite('s')  # Memilih opsi "Size"
-    time.sleep(0.5)
-    pyautogui.press('enter')  # Menjalankan perintah "Size"
-    time.sleep(0.5)
-    pyautogui.typewrite(f"{width},{height}")  # Menulis ukuran yang diinginkan
-    time.sleep(0.5)
-    pyautogui.press('enter')  # Menjalankan perintah "Size"
-
 def read_youtube_urls_from_file(file_path):
     with open(file_path, 'r') as file:
         return file.readlines()
@@ -36,25 +15,13 @@ def read_youtube_urls_from_file(file_path):
 if __name__ == "__main__":
     # Gantilah dengan nama file yang berisi daftar URL video YouTube
     file_path = "yt_list.txt"
-    
-    # Mengatur ukuran jendela Chrome
-    resize_chrome(300, 100)
 
     while True:
-        # Membaca daftar URL dari file
-        youtube_urls = read_youtube_urls_from_file(file_path)
-
-        # Memeriksa apakah daftar URL kosong
-        if not youtube_urls:
-            print("Daftar URL kosong. Mulai dari awal.")
-            continue
-
-        # Melakukan perulangan setiap 30 detik sebanyak 5 kali
-        for url in youtube_urls:
-            minimize_chrome()  # Meminimalkan jendela Chrome sebelum membuka URL berikutnya
-            url_with_params = f"{url.strip()}&autoplay=1&mute=1&loop=1"
-            open_chrome_as_guest(url_with_params)  # Menghapus karakter newline dan spasi ekstra
-            time.sleep(30)  # Menunggu 30 detik sebelum iterasi selanjutnya
-
-        # Setelah selesai membaca seluruh daftar, kembali ke awal
-        print("Selesai membaca daftar. Mulai dari awal.")
+    # Membaca daftar URL dari file
+    youtube_urls = read_youtube_urls_from_file(file_path)
+    
+    # Melakukan perulangan setiap 5 detik sebanyak 5 kali
+    for url in youtube_urls:
+        url_with_params = f"{url.strip()}&autoplay=1&mute=1&loop=1"
+        open_chrome_as_guest(url_with_params)  # Menghapus karakter newline dan spasi ekstra
+        time.sleep(5)  # Menunggu 5 detik sebelum iterasi selanjutnya
