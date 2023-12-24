70784c9ad9facf819705d737b61322f31d434cbd
InyongNation <71963003+inyongnation@users.noreply.github.com>
2023-12-24 09:16:49 +0800

    Update play.py

--- play.py
+++ play.py
@@ -1,16 +1,26 @@
 import subprocess
+import time
 
 def open_chrome_as_guest(video_url):
     try:
-        # Membuka Chrome sebagai tamu, memutar video YouTube, dan mengatur autoplay dan mute
+        # Membuka Chrome sebagai tamu, memutar video YouTube, dan mengatur autoplay, mute, dan loop
         subprocess.run(["google-chrome", "--guest", "--new-window", video_url], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
+def read_youtube_urls_from_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
 if __name__ == "__main__":
-    # Gantilah URL dengan URL video YouTube yang ingin Anda buka
-    youtube_video_url = "https://www.youtube.com/watch?v=XvkEiIwt-UU&autoplay=1&mute=1"
+    # Gantilah dengan nama file yang berisi daftar URL video YouTube
+    file_path = "yt_list.txt"
     
-    # Melakukan perulangan 5 kali
-    for _ in range(5):
-        open_chrome_as_guest(youtube_video_url)
+    # Membaca daftar URL dari file
+    youtube_urls = read_youtube_urls_from_file(file_path)
+    
+    # Melakukan perulangan setiap 5 detik sebanyak 5 kali
+    for url in youtube_urls:
+        url_with_params = f"{url.strip()}&autoplay=1&mute=1&loop=1"
+        open_chrome_as_guest(url_with_params)  # Menghapus karakter newline dan spasi ekstra
+        time.sleep(5)  # Menunggu 5 detik sebelum iterasi selanjutnya
